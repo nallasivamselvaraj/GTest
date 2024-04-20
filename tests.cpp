bbe64b23795e37ce2fe6f027760bc7cd64fd1850
nallasivamselvaraj <147471474+nallasivamselvaraj@users.noreply.github.com>
2024-04-21 01:15:01 +0530

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,19 +1,18 @@
 #include "whattotest.cpp"
 #include <gtest/gtest.h>
 
-TEST(SquareRootTest, PositiveNos) { 
-    ASSERT_EQ(6, squareRoot(36.0));
-    ASSERT_EQ(18.0, squareRoot(324.0));
-    ASSERT_EQ(25.4, squareRoot(645.16));
-    ASSERT_EQ(0, squareRoot(0.0));
+TEST(AddTest, PositiveNumbers) { 
+    ASSERT_EQ(10, add(5));
+    ASSERT_EQ(15, add(10));
 }
 
-TEST(SquareRootTest, NegativeNos) {
-    ASSERT_EQ(-1.0, squareRoot(-15.0));
-    ASSERT_EQ(-1.0, squareRoot(-0.2));
+TEST(AddTest, NegativeNumbers) {
+    ASSERT_EQ(0, add(-5));
+    ASSERT_EQ(-3, add(-8));
 }
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
