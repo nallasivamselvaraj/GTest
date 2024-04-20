b9dd8b9110e43e3aa06558dec62d5ace7ca44eac
nallasivamselvaraj <147471474+nallasivamselvaraj@users.noreply.github.com>
2024-04-21 01:18:19 +0530

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -2,12 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(10, int(10));
+  EXPECT_EQ(15, int(10));
   EXPECT_TRUE(true);
-}
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
 
